542eedb033ff158e85d8e2aa45636d3f6e6a0ae1
cph101 <cph101@icloud.com>
2023-12-05 10:40:10 +0000

    Added settings icon and tweaked css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class',
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  darkMode: "class",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
@@ -15,46 +13,54 @@ export default {
           "base-100": "#e3e4eb",
           "neutral-content": "#555",
           ".footer": {
-            "background-color":"white",
+            "background-color": "white",
           },
           ".text-link": {
-            "color": "#2140ba",
+            color: "#2140ba",
           },
         },
         dark: {
           ...require("daisyui/src/theming/themes")["dark"],
-          "base-100":"#333",
+          "base-100": "#333",
           ".footer": {
-            "background-color":"#222",
+            "background-color": "#222",
           },
           ".text-link": {
-            "color": "#66b3ff",
+            color: "#66b3ff",
           },
         },
-         crimson: {
-
-       "primary": "#813151",
-       "secondary": "#672741",
-       "accent": "#00aeff",
-       "neutral": "#0e0a0d",
-       "base-100": "#331320",
-       "info": "#0089ff",
-       "success": "#006666",
-       "warning": "#ff9400",
-       "error": "#ff284a",
-           ".footer": {
-             "background-color":"#4d1d31",
-           },
-           ".text-link": {
-             "color": "#66b3ff",
-           },
-         },
-       }
+        crimson: {
+          primary: "#813151",
+          secondary: "#672741",
+          accent: "#00aeff",
+          neutral: "#0e0a0d",
+          "base-100": "#331320",
+          info: "#0089ff",
+          success: "#006666",
+          warning: "#ff9400",
+          error: "#ff284a",
+          ".footer, .btn": {
+            "background-color": "#4d1d31",
+            "border-color": "#4d1d31",
+          },
+          ".btn:hover": {
+            "background-color": "#451a2c",
+            "border-color": "#451a2c",
+          },
+          ".text-link": {
+            color: "#66b3ff",
+          },
+        },
+      },
     ],
   },
   corePlugins: {
     aspectRatio: false,
   },
-  plugins: [require('@tailwindcss/aspect-ratio'),require('@tailwindcss/forms'),require('@tailwindcss/aspect-ratio'),require("daisyui"),],
-}
-
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("daisyui"),
+  ],
+};
